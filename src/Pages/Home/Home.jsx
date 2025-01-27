ff28cc5e2ccc7b890c4ac996a31d7650f8730237
mdanamulhasan201 <anamul15-3625@diu.edu.bd>
2025-01-27 11:46:29 +0600

    added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import About from '../../Components/Home/About'
+import Banner from '../../Components/Home/Banner'
+import ChooseUs from '../../Components/Home/ChooseUs'
+import FeatureOpportunities from '../../Components/Home/FeatureOpportunities'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <FeatureOpportunities />
+      <About />
+      <ChooseUs />
+    </div>
+  )
+}
+
+export default Home
